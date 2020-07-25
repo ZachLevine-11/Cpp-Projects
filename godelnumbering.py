eb6eb3c79a866ed8169b5a9d23010c4fd29e1cbf
Zach-Levine <levinez@mcmaster.ca>
2020-07-25 03:36:42 -0400

    Update godelnumbering.py

--- godelnumbering.py
+++ godelnumbering.py
@@ -6,17 +6,19 @@ def primes_upto(dd):
             for c in range(n*n, dd, n):
                 prime[c] = False
 
+s = "abc"
+
 letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 ps = {}
-primes = list(primes_upto(542))
+primes = list(primes_upto(len(s)))
 for i in range(1, len(letters)):
     ps[letters[i]] = i
 ps["a"] = 27
 
 def givesentence(s):
-    ddd = 1
+    sentnum = 1
     for i in range(len(s)):
-        yeet = yeet*primes[ps[s[i]]]
-    return(ddd)
+        sentnum = sentnum*primes[ps[s[i]]]
+    return(sentnum)
 
     
