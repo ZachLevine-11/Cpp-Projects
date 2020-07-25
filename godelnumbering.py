3aa2c6e97072e8bc881ed7c27b5ace4f0d03d49c
Zach-Levine <levinez@mcmaster.ca>
2020-07-25 00:59:38 -0400

    Update godelnumbering.py

--- godelnumbering.py
+++ godelnumbering.py
@@ -1,10 +1,10 @@
-def primes_upto(limit):
-    prime = [True] * limit
-    for n in range(2, limit):
+def primes_upto(dd):
+    prime = [True] * dd
+    for n in range(2, dd):
         if prime[n]:
-            yield n # n is a prime
-            for c in range(n*n, limit, n):
-                prime[c] = False # mark composites
+            yield n
+            for c in range(n*n, dd, n):
+                prime[c] = False
 
 letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 ps = {}
@@ -14,9 +14,9 @@ for i in range(1, len(letters)):
 ps["a"] = 27
 
 def givesentence(s):
-    yeet = 1
+    ddd = 1
     for i in range(len(s)):
         yeet = yeet*primes[ps[s[i]]]
-    return(yeet)
+    return(ddd)
 
 print(givesentence("greg"))
